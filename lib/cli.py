e966c5301c015ffc50680a414ca204e7c015c93f
luis <servinluis21@yahoo.com>
2024-02-04 13:55:15 -0500

    done

--- lib/cli.py
+++ lib/cli.py
@@ -1,75 +1,94 @@
-
-from helpers import (
-    exit_program,
-    list_departments,
-    find_department_by_name,
-    find_department_by_id,
-    create_department,
-    update_department,
-    delete_department,
-    list_employees,
-    find_employee_by_name,
-    find_employee_by_id,
-    create_employee,
-    update_employee,
-    delete_employee,
-    list_department_employees
-)
+from models.department import Department
+from models.employee import Employee
 
 
-def main():
-    while True:
-        menu()
-        choice = input("> ")
-        if choice == "0":
-            exit_program()
-        elif choice == "1":
-            list_departments()
-        elif choice == "2":
-            find_department_by_name()
-        elif choice == "3":
-            find_department_by_id()
-        elif choice == "4":
-            create_department()
-        elif choice == "5":
-            update_department()
-        elif choice == "6":
-            delete_department()
-        elif choice == "7":
-            list_employees()
-        elif choice == "8":
-            find_employee_by_name()
-        elif choice == "9":
-            find_employee_by_id()
-        elif choice == "10":
-            create_employee()
-        elif choice == "11":
-            update_employee()
-        elif choice == "12":
-            delete_employee()
-        elif choice == "13":
-            list_department_employees()
-        else:
-            print("Invalid choice")
+def exit_program():
+    print("Goodbye!")
+    exit()
+
+# We'll implement the department functions in this lesson
+
+def list_departments():
+    departments = Department.get_all()
+    for department in departments:
+        print(department)
 
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. List all departments")
-    print("2. Find department by name")
-    print("3. Find department by id")
-    print("4: Create department")
-    print("5: Update department")
-    print("6: Delete department")
-    print("7. List all employees")
-    print("8. Find employee by name")
-    print("9. Find employee by id")
-    print("10: Create employee")
-    print("11: Update employee")
-    print("12: Delete employee")
-    print("13: List all employees in a department")
+def find_department_by_name():
+    name = input("Enter the department's name: ")
+    department = Department.find_by_name(name)
+    print(department) if department else print(
+        f'Department {name} not found')
 
 
-if __name__ == "__main__":
-    main()
+def find_department_by_id():
+    #use a trailing underscore not to override the built-in id function
+    id_ = input("Enter the department's id: ")
+    department = Department.find_by_id(id_)
+    print(department) if department else print(f'Department {id_} not found')
+
+
+def create_department():
+    name = input("Enter the department's name: ")
+    location = input("Enter the department's location: ")
+    try:
+        department = Department.create(name, location)
+        print(f'Success: {department}')
+    except Exception as exc:
+        print("Error creating department: ", exc)
+
+
+def update_department():
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        try:
+            name = input("Enter the department's new name: ")
+            department.name = name
+            location = input("Enter the department's new location: ")
+            department.location = location
+
+            department.update()
+            print(f'Success: {department}')
+        except Exception as exc:
+            print("Error updating department: ", exc)
+    else:
+        print(f'Department {id_} not found')
+
+
+def delete_department():
+    id_ = input("Enter the department's id: ")
+    if department := Department.find_by_id(id_):
+        department.delete()
+        print(f'Department {id_} deleted')
+    else:
+        print(f'Department {id_} not found')
+
+
+# You'll implement the employee functions in the lab
+
+def list_employees():
+    pass
+
+
+def find_employee_by_name():
+    pass
+
+
+def find_employee_by_id():
+    pass
+
+
+def create_employee():
+    pass
+
+
+def update_employee():
+    pass
+
+
+def delete_employee():
+    pass
+
+
+def list_department_employees():
+    pass
